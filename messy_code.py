d43d320fdbb6e06b74d93ee8a2d80572c4d738c3
L202-40 <l202-40@admins-iMac.local>
2025-10-25 11:52:54 +0800

    first commit

--- messy_code.py
+++ messy_code.py
@@ -1,11 +1,15 @@
 #this program add numbers and print the result but its really messy
+def addnumb(a,b):
+  return a+b
 
-def addnumb(a,b):return a+b
-def main( ):
-  print("This is a simple adder program")
-  a=input("enter first number")
-  b=input("enter second number")
+def main():
+  print("\nThis is a simple adder program\n")
+  a=input("Enter first number: ")
+  b=input("Enter second number: ")
+
   res=addnumb(int(a),int(b))
-  print("the sum is :",res)
+  print("\nResult: ",res)
+
+  
 main( )#call the function at end
 # KELVIN CODE
